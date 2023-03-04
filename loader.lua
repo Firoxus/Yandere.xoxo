a4a3bdaf18ba9a32b7013ff0049dd6a6e2553e29
Firox <86130692+Firoxus@users.noreply.github.com>
2023-03-04 15:00:06 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -74,5 +74,5 @@ wait(0.3)
 Frame.Transparency = 0.900
 wait(0.3)
 Frame.Transparency = 1
-Frame.Visible = false
+Frame:Destroy()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Yandere.xoxo/main/yandere.lua"))()
