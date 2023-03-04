2dc5136577c3127ea8c0931509c1a6f388a9570d
Firox <86130692+Firoxus@users.noreply.github.com>
2023-03-04 14:51:48 +0000

    Create yandere.lua

---
+++ yandere.lua
@@ -0,0 +1,31 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Yandere.xoxo/main/Lib/Cattoware.lua"))()
+local game = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local user = game.Players.LocalPlayer.DisplayName
+local Window = Library:CreateWindow("Yandere.xoxo | " .. user .. " | " .. game, Vector2.new(420, 420), Enum.KeyCode.RightControl) 
+
+-- Tab system?? --
+local AimingTab = Window:CreateTab("Aiming") 
+local VisualsTab = Window:CreateTab("Visuals")
+local MiscTab = Window:CreateTab("Misc") 
+local AntiTab = Window:CreateTab("Anti-Aim") 
+local UserTab = Window:CreateTab("User-Interface") 
+
+-- Aiming section --
+local SilentSection = AimingTab:CreateSector("Silent-Aim", "left")
+local AssistSection = AimingTab:CreateSector("Aim-Assist", "right")
+
+-- Visual section --
+local EspSection = VisualsTab:CreateSector("Esp", "left")
+local ChamsSection = VisualsTab:CreateSector("Chams", "right")
+
+-- Misc section --
+local WeaponSection = MiscTab:CreateSector("Weapon-Modifications", "left")
+local MacroSection = MiscTab:CreateSector("Macros", "right")
+
+-- Anti Section --
+local VelocityAntis = AntiTab:CreateSector("Velocity-AntiAims", "left")
+local BlatantAntis = AntiTab:CreateSector("Blatant-AntiAims", "right")
+
+-- Ui tab --
+local ConfigSystem = UserTab:CreateSector("Configuration", "left")
+local UserInterface = UserTab:CreateSector("User-Interface", "right")
